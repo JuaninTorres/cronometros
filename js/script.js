a36985d474614f7a696732e44b7b247c8b7b38bb
Juan Torres <juanin.torres@gmail.com>
2012-08-26 19:24:41 -0400

    Ranking Funcional
    Ya funcionan los cronometros, pero aun falta trabajar en el ranking

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 
 */
 
-var tiempoGeneral = parseInt(0);
+var tiempoGeneral = parseInt(0,10);
 var cronometroEnEjecucion = false;
+var resultado = [];
 
 function formatearTiempo(t){
     // El tiempo viene en milisegundos
-    var m_tmp   = parseInt(t);
+    var m_tmp   = parseInt(t,10);
     var horas   = Math.floor(m_tmp/ 3600000);
     var minutos = Math.floor((m_tmp - (horas * 3600000)) / 60000);
     var segundos = Math.floor((m_tmp - (horas * 3600000) - (minutos * 60000))/1000);
@@ -20,17 +21,47 @@ function formatearTiempo(t){
     return time;
 }
 
+function removeA(arr){
+    var what, a= arguments, L= a.length, ax;
+    while(L> 1 && arr.length){
+        what= a[--L];
+        while((ax= arr.indexOf(what))!= -1){
+            arr.splice(ax, 1);
+        }
+    }
+    return arr;
+}
+
 function cronometroPrincipal()
 {
     $("#cronometroPrincipal").html(formatearTiempo(tiempoGeneral));
     if(cronometroEnEjecucion)
     {
-    	tiempoGeneral = tiempoGeneral +100;
-    	setTimeout("cronometroPrincipal()",100);
+        tiempoGeneral = tiempoGeneral+100;
+        setTimeout("cronometroPrincipal()",100);
     }
-
 }
 
-function iniciarCronometro(){cronometroEnEjecucion = true;cronometroPrincipal();}
-function detenerCronometro(){cronometroEnEjecucion = false;}
-function resetCronometro(){cronometroEnEjecucion = false;tiempoGeneral=0;cronometroPrincipal();}
+function detenerRelojEquipo(id){
+    $("#reloj"+id).html(formatearTiempo(tiempoGeneral));
+    $("#tiempo"+id).val(tiempoGeneral);
+
+    if($.inArray(id,resultado)!=-1){
+        removeA(resultado,id);
+    }
+    resultado.push(id);
+}
+
+function iniciarCronometro(ejecutando){
+    if(!ejecutando)
+    {
+        cronometroEnEjecucion=true;
+        cronometroPrincipal();
+    }
+    else
+    {
+        cronometroEnEjecucion=false;
+    }
+}
+function detenerCronometro(){$("#btnIniciar").removeAttr("disabled");cronometroEnEjecucion=false;}
+function resetCronometro(){$("#btnIniciar").removeAttr("disabled");cronometroEnEjecucion=false;tiempoGeneral=0;cronometroPrincipal();}
